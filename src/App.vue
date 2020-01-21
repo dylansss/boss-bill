ae29576f9981c669175388a7bc016cb1f99bbc35
dylansss <939197294@qq.com>
2020-01-21 11:13:17 +0800

    基础功能

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,58 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<div>
+  <el-button type="primary" @click="dialogVisible = true">记账</el-button>
+  <el-table
+    :data="tableData"
+    stripe
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="money"
+      label="金额">
+    </el-table-column>
+    <el-table-column
+      prop="memo"
+      label="备注">
+    </el-table-column>
+  </el-table>
+  <add-modal :dialogVisible="dialogVisible" @onSubmit="onSubmit"></add-modal>
+</div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import addModal from './views/add-modal'
+export default {
+  components: {
+    addModal
+  },
+  data () {
+    return {
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          money: '11111',
+          memo: '备注'
+        }
+      ],
+      dialogVisible: false
+    }
+  },
+  methods: {
+    onSubmit (params) {
+      console.log(params)
+      this.dialogVisible = false
+      this.tableData.push(params)
     }
   }
 }
-</style>
+</script>
---
+++ src/views/add-modal.vue
@@ -0,0 +1,81 @@
+<template>
+  <el-dialog
+    title="提示"
+    :visible.sync="dialogVisible"
+    width="50%">
+    <el-form ref="form"  :rules="rules" :model="form" label-width="80px">
+      <el-form-item label="姓名">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="日期">
+        <el-date-picker
+        type="date"
+        format="yyyy 年 MM 月 dd 日"
+        value-format="yyyy-MM-dd"
+        placeholder="选择日期"
+        v-model="form.date"
+        style="width: 100%;"></el-date-picker>
+      </el-form-item>
+      <el-form-item label="金额">
+        <el-input v-model="form.money"></el-input>
+      </el-form-item>
+      <el-form-item label="欠款状态">
+        <el-radio-group v-model="form.status">
+          <el-radio label="0">已还清</el-radio>
+          <el-radio label="1">部分还清</el-radio>
+          <el-radio label="2">未还</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="备注">
+        <el-input v-model="form.memo"></el-input>
+      </el-form-item>
+      <!-- <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button>取消</el-button>
+      </el-form-item> -->
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="onSubmit">取 消</el-button>
+      <el-button type="primary" @click="onSubmit">确 定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: 'add-moadl',
+  props: ['dialogVisible'],
+  data () {
+    return {
+      form: {
+        name: '',
+        date: new Date(),
+        money: '',
+        memo: '',
+        status: '2'
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入姓名', trigger: 'blur' }
+        ],
+        data: [
+          { required: true, message: '请选择日期', trigger: 'blur' }
+        ],
+        money: [
+          { required: true, message: '请输入金额', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      // debugger
+      this.$emit('onSubmit', this.form)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
