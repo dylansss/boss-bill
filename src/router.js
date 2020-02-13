0972b57ce6a6ef301fe398cd0a488eef1866681f
dylansss <939197294@qq.com>
2020-02-13 10:33:57 +0800

    找个云盘把日报存进去

--- src/router.js
+++ src/router.js
@@ -8,27 +8,27 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: 'home-web'
+      redirect: 'home'
     },
     {
       path: '/home',
       name: 'home',
-      component: () => import('./views/home')
+      component: () => import(/* webpackChunkName: "home" */ './views/home')
     },
     {
       path: '/home-web',
       name: 'home-web',
-      component: () => import('./views/home-web')
+      component: () => import(/* webpackChunkName: "home-web" */ './views/home-web')
     },
     {
       path: '/h5-detail',
       name: 'h5-detail',
-      component: () => import('./views/h5-detail')
+      component: () => import(/* webpackChunkName: "h5-detail" */ './views/h5-detail')
     },
     {
       path: '/h5-add',
       name: 'h5-add',
-      component: () => import('./views/h5-add')
+      component: () => import(/* webpackChunkName: "h5-add" */ './views/h5-add')
     }
   ]
 })
